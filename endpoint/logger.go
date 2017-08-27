581649d1579f41706b715247d2ca246ca780b3c5
IggyBlob <socialmedia@paulhaunschmied.com>
2017-08-27 18:17:06 +0200

    Log remote IP adress

--- endpoint/logger.go
+++ endpoint/logger.go
@@ -11,11 +11,12 @@ func logger(inner http.Handler, name string) http.Handler {
 		start := time.Now()
 		inner.ServeHTTP(w, r)
 		log.Printf(
-			"%s\t%s\t%s\t%s",
+			"%s\t%s\t%s\t%s\t%s",
 			r.Method,
 			r.RequestURI,
 			name,
 			time.Since(start),
+			r.RemoteAddr,
 		)
 	})
 }
